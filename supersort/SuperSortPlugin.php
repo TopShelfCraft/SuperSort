dedc799f03cbe398bf394853b08c99e963647de3
Michael Rog <michael@michaelrog.com>
2015-01-24 10:42:07 -0600

    Beta!

---
+++ supersort/SuperSortPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @package   SuperSort
+ * @author    Michael Rog
+ * @copyright Copyright 2014
+ * @link      http://michaelrog.com/craft/supersort
+ */
+
+namespace Craft;
+
+class SuperSortPlugin extends BasePlugin
+{
+
+    function getName()
+    {
+         return Craft::t('SuperSort');
+    }
+
+    function getVersion()
+    {
+        return '0.0.1';
+    }
+
+    function getDeveloper()
+    {
+        return 'Michael Rog';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'http://michaelrog.com/craft';
+    }
+
+    function addTwigExtension()
+    {
+        Craft::import('plugins.supersort.twigextensions.SuperSortTwigExtension');
+        return new SuperSortTwigExtension();
+    }
+
+}
