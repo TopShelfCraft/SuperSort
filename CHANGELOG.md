0f8375f35f1e561aa56533c9c968c1df85f327b1
TJ Draper <tj@buzzingpixel.com>
2017-09-18 13:27:11 -0500

    Updated SuperSort for Craft 3.
    - Add standard gitignore
    
    - Add editor config and PHP MD/CS rules
    
    - Add composer file
    
    - Add Craft as requirement of composer (make development in PHPStorm easier for one thing)
    
    - Add initial base plugin class
    
    - Update info in composer.json
    
    - Move the plugin icon into the correct place for Craft 3
    
    - Add new changelog format
    
    - Remove old releases.json format
    
    - Update composer.json
    
    - Update Twig extension to work with Craft 3
    
    - Remove old plugin file
    
    - Update readme
    
    - Update methods and objects for Craft 3

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# SuperSort Changelog
+
+## Unreleased
+### New
+- Refactored for Craft 3 support
+
+## 1.1.0 - 2015-11-05
+### Added
+- Added the `shuffle` method.
+
+## 1.0.0 - 2015-11-05
+### Fixed
+- Fixed a bug where the `rsortas` method was erroneously set to sort the source array rather than the transformed `asArray`, causing incorrect results.
+
+## 0.0.1 - 2015-01-24
+- Beta release
+
---
+++ src/SuperSort.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace topshelfcraft\supersort;
+
+use Craft;
+use craft\base\Plugin;
+
+/**
+ * Class SuperSort
+ */
+class SuperSort extends Plugin
+{
+    /** @var SuperSort $plugin */
+    public static $plugin;
+
+    /**
+     * Initialize plugin
+     */
+    public function init()
+    {
+        // Make sure parent init functionality runs
+        parent::init();
+
+        // Save an instance of this plugin for easy reference throughout app
+        self::$plugin = $this;
+
+        // Add the twig extension
+        Craft::$app->view->twig->addExtension(
+            new SuperSortTwigExtension()
+        );
+    }
+}
